392561666262f2f526279020a823363f89d4f61d
Ravi Kumar Singh <imrk016@gmail.com>
2021-07-13 07:16:01 +0530

    adding login screen

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 
+const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
 
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
 
+const spinner = document.getElementById("spinner");
+
+function showSpinner() {
+    spinner.style ="display:block;"
+  
+}
+
+function hideSpinner() {
+    spinner.style ="display:none;"
+   
+}
+
+var JWT_Token ;
+
 
 
 
 function begin() {
-   
+    showSpinner();
+
     var data = {};
     data.username = $('#username').val();
     data.password = $('#password').val()
@@ -17,7 +38,7 @@ function begin() {
     $.ajax({
         type: "POST",
         datatype:"application/json",
-        url: "/login",
+        url: "http://127.0.0.1:5000/login",
        
         data: JSON.stringify(data),
         contentType: 'application/json; charset=utf-8',
