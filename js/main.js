ce5f6d2a88dde95dca0dae9072b9f9696b8f4aec
Ravi Kumar Singh <imrk016@gmail.com>
2021-07-13 18:42:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,16 +46,61 @@ function begin() {
         success: (data) => { 
             // console.log(`JWT TOken : ${data}`)
             hideSpinner();
-            JWT_Token =`${data}`;
+            JWT_Token =`${data.access_token}`;
 
             localStorage.setItem("SavedToken",JWT_Token);
             axios.defaults.headers.common['Authorization'] = JWT_Token;
           
 
 
-            window.location.href = 'test.html';
+            //window.location.href = 'home.html';
            
-            console.log(JWT_Token)
+            console.log(data.message)
+            console.log(data.access_token)
+            
+          
+        },
+        error: function(xhr, status, error) {
+            hideSpinner();
+            console.log(`ERROR`)
+            document.getElementById('error-output').style ="display:block;margin:auto!important;margin: 15px 0!important;";
+            document.getElementById('error-output').textContent = `Error : Username or Password not correct!`;
+        }
+    });
+    
+  
+
+}
+
+
+function signup() {
+    showSpinner();
+
+    var data = {};
+    data.email = $('#email').val();
+    data.password = $('#password').val()
+
+    $.ajax({
+        type: "POST",
+        datatype:"application/json",
+        url: "https://vbudget-apis.herokuapp.com/login",
+       
+        data: JSON.stringify(data),
+        contentType: 'application/json; charset=utf-8',
+        
+        success: (data) => { 
+            // console.log(`JWT TOken : ${data}`)
+            hideSpinner();
+            JWT_Token =`${data.access_token}`;
+
+            localStorage.setItem("SavedToken",JWT_Token);
+            axios.defaults.headers.common['Authorization'] = JWT_Token;
+          
+
+
+            window.location.href = 'home.html';
+           
+            console.log(data.message)
             
           
         },
